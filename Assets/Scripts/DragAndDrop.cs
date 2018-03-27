f0e5cffbed66f4ce59fe1d11c1610ec67ff7e840
Malius Arth <dev.malius@chello.at>
2018-03-27 20:29:26 +0200

    moved DragAndDrop.cs to Scripts and fixed Collider requirement

---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class DragAndDrop : MonoBehaviour
+{
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	void OnMouseDown()
+	{
+		Debug.Log("DragAndDrop: OnMouseDown");
+		offset = gameObject.transform.position -
+		         Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+}
