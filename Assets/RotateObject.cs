6252bbc666e7ef6d16325242b20e568b12622387
Nikola Dragovic <nikoladragovic@hotmail.com>
2018-03-27 17:52:59 +0200

    Implementation of Object rotation of collidable objects

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+
+//add this component to camera
+public class RotateObject : MonoBehaviour {
+
+    public static Transform selected;
+
+    public Camera camera;
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            
+            Vector2 origin =new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.zero, 0f);
+
+            if(hit) {
+                selected = hit.transform;
+            }
+
+        }
+
+        if(Input.GetKey(KeyCode.A) && selected) {
+                selected.Rotate(Vector3.forward*40*Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.D) && selected) {
+                selected.Rotate(-Vector3.forward*40*Time.deltaTime);
+        }
+        
+    }
+
+
+    
+
+}
