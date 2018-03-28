d71e7dc3c875ba81ce1d5703da5f1a72958a23e6
Authix <paul.groeger@hotmail.de>
2018-03-28 14:29:46 +0200

    Big Update on Pause Menu
    Added Music with Musicmanager and VolumeSlider
    fixed some Buttons

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject MenuImage;
+    public GameObject SettingsButton;
+    public GameObject ExitSettingsButton;
+    public GameObject ContinueButton;
+    public GameObject QuitButton;
+    public GameObject Slider;
+
+    void Start()
+    {
+        Button btn1 = SettingsButton.GetComponent<Button>();
+        btn1.onClick.AddListener(TaskOnClick);
+        Button btn2 = ExitSettingsButton.GetComponent<Button>();
+        btn2.onClick.AddListener(TaskOnClick1);
+        Button btn3 = ContinueButton.GetComponent<Button>();
+        btn3.onClick.AddListener(TaskOnClick1);
+        Button btn4 = ContinueButton.GetComponent<Button>();
+        btn4.onClick.AddListener(TaskOnClick2);
+
+    }
+
+    void TaskOnClick()
+    {
+        SettingsButton.SetActive(false);
+        ExitSettingsButton.SetActive(true);
+        MenuImage.SetActive(true);
+        ContinueButton.SetActive(true);
+        QuitButton.SetActive(true);
+        Slider.SetActive(true);
+
+    }
+    void TaskOnClick1()
+    {
+        SettingsButton.SetActive(true);
+        ExitSettingsButton.SetActive(false);
+        MenuImage.SetActive(false);
+        ContinueButton.SetActive(false);
+        QuitButton.SetActive(false);
+        Slider.SetActive(false);
+    }
+    void TaskOnClick2()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioSource MyAudioSource;
+    public Slider VolumeSlider;
+    //float MySliderValue;
+
+    void Start()
+    {
+        //Initiate the Slider value to half way
+        //MySliderValue = 0.7f;
+        VolumeSlider.value = 0.7f;
+        //Fetch the AudioSource from the GameObject
+        MyAudioSource = GetComponent<AudioSource>();
+        //Play the AudioClip attached to the AudioSource on startup
+        MyAudioSource.Play();
+    }
+
+    void Update()
+    {
+        
+        MyAudioSource.volume = VolumeSlider.value;
+    }
+}
